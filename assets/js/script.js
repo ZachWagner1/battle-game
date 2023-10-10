3fb507ec98e673326fc2009cc3aa426b049c1020
Zachary Wagner <zwag14@yahoo.com>
2023-10-10 11:48:56 -0500

    basic stats and welcome

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var playerName = window.prompt("Name your character!");
+var playerHP = 1000;
+var playerAttack = 50;
+
+var opponentName = "Gus"
+var opponentHP = 350;
+var opponentAttack = 15;
+
+var fight = function() {
+    window.alert("Welcome to the Battle Games!");
+};
+
+fight();
